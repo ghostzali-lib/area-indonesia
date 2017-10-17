603d15cd50f0bdd919578534ae09c4d13e8f9c5a
Alexander Schnebel <schnebel.alexander@gmail.com>
2017-10-17 19:55:17 +0200

    Fixed bug.

--- src/main/java/id/goindonesia/area/indonesia/AreaIndonesiaApplication.java
+++ src/main/java/id/goindonesia/area/indonesia/AreaIndonesiaApplication.java
@@ -5,10 +5,14 @@ import id.goindonesia.area.indonesia.setup.DataImport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class AreaIndonesiaApplication {
 
     @Autowired
